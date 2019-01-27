0051d418e37776ebb3be38a55895bbd6281e6dc1
vanntile <vanntile@protonmail.com>
2019-01-27 19:16:15 +0200

    Day 4

--- 03 - CSS Variables/script.js
+++ 03 - CSS Variables/script.js
@@ -1,6 +1,6 @@
 const inputs = document.querySelectorAll('.controls input')
 
-var updateHandler = function() {
+var updateHandler = function () {
   const suffix = this.dataset.sizing || ''
   document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix)
 }
