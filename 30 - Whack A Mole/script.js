6658fd72de0d612b70a890d07a4f87f4b0e34986
vanntile <vanntile@protonmail.com>
2019-02-11 16:54:42 +0200

    Day 30

---
+++ 30 - Whack A Mole/script.js
@@ -0,0 +1,69 @@
+const holes = document.querySelectorAll('.hole')
+const scoreBoard = document.querySelector('.score')
+const title = document.querySelector('.title')
+const moles = document.querySelectorAll('.mole')
+const startButton = document.querySelector('button')
+
+const min = 200 // minimum time in miliseconds
+const max = 1000 // maximum time in miliseconds
+const duration = 20 // maximum game time in seconds
+
+let lastHole
+let timeUp = true
+let score
+
+const randomTime = (min, max) => {
+  return Math.round(Math.random() * (max - min) + min)
+}
+
+const randomHole = (holes) => {
+  const idx = Math.floor(Math.random() * holes.length)
+  const hole = holes[idx]
+
+  if (hole === lastHole) {
+    return randomHole(holes)
+  }
+
+  lastHole = hole
+  return hole
+}
+
+const peep = () => {
+  const time = randomTime(min, max)
+  const hole = randomHole(holes)
+
+  hole.classList.add('up')
+  setTimeout(() => {
+    hole.classList.remove('up')
+
+    if (!timeUp) {
+      peep()
+    } else {
+      title.textContent = 'Your score is'
+    }
+  }, time)
+}
+
+const startGame = () => {
+  score = 0
+  scoreBoard.textContent = 0
+  title.textContent = 'Whack-a-mole!'
+  timeUp = false
+
+  peep()
+  setTimeout(() => {
+    timeUp = true
+  }, (duration * 1000))
+}
+
+const bonk = function (e) {
+  if (!e.isTrusted) return
+
+  score++
+  this.classList.remove('up')
+  scoreBoard.textContent = score
+}
+
+moles.forEach(mole => mole.addEventListener('click', bonk))
+
+startButton.addEventListener('click', () => timeUp && startGame())
--- README.md
+++ README.md
@@ -50,4 +50,4 @@ Check the course at [https://JavaScript30.com](https://JavaScript30.com)
 27. [x] [Click and Drag](https://vanntile.github.io/JavaScript30/27%20-%20Click%20and%20Drag)
 28. [x] [Video Speed Controller](https://vanntile.github.io/JavaScript30/28%20-%20Video%20Speed%20Controller)
 29. [x] [Countdown Timer](https://vanntile.github.io/JavaScript30/29%20-%20Countdown%20Timer)
-30. [ ] Whack A Mole
+30. [x] [Whack A Mole](https://vanntile.github.io/JavaScript30/30%20-%20Whack%20A%20Mole)
