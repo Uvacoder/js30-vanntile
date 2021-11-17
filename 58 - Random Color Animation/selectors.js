cbcb875bb9830c676a1192f0ac1733624e4d267b
Uvacoder <88749643+Uvacoder@users.noreply.github.com>
2021-11-17 07:50:52 -0500

    Create selectors.js

---
+++ 58 - Random Color Animation/selectors.js
@@ -0,0 +1,9 @@
+// Moduled Selectors for ID
+function $(id) {
+  return document.getElementById(id);
+}
+
+// Moduled Selectors for querySelector
+function Q(q) {
+  return document.querySelector(q);
+}
